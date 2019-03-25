252cd190d0fe61957142f11360c51cf731b295ce
Moritz <44192283+Moritzheine@users.noreply.github.com>
2019-03-25 16:17:02 +0100

    initial commit

---
+++ sht31.ts
@@ -0,0 +1,57 @@
+/**
+ * Functions for the sht31 temp and humidity sensor and the Calliope mini
+ *
+ * @author Moritz Heine
+ */
+
+//% weight=3 color=#97699c block="sht31"
+namespace sht31 {
+    let ADDRESS = 0x44;
+    let temp = 0;
+    let humidity = 0;
+
+    function readTempHum(): boolean {
+        let tmp_buf = 0
+        let hum_buf = 0
+        pins.i2cWriteNumber(ADDRESS, 0x2400, NumberFormat.UInt16BE)
+        basic.pause(100)
+
+        let buf = pins.i2cReadBuffer(ADDRESS, 6, false)
+        if (buf.length != 6) {
+            return false
+        }
+        tmp_buf = buf[0] << 8
+        tmp_buf |= buf[1]
+
+        hum_buf = buf[3] << 8
+        hum_buf |= buf[4]
+
+        temp = tmp_buf * 175 / (0xffff - 1) - 45
+        humidity = hum_buf * 100 / (0xffff - 1)
+        return true
+    }
+
+    /**
+     * Read temp
+     */
+    //% blockId=readTemp
+    //% block="read temp"
+    export function readTemp(): number {
+        if (!readTempHum()) {
+            return 0
+        }
+        return temp
+    }
+
+    /**
+     * Read humidity
+     */
+    //% block="read humidity"
+    //% blockId=digitalRead
+    export function readHum(): number {
+        if (!readTempHum()) {
+            return 0
+        }
+        return humidity
+    }
+}
